cd55139d7282c2feabdb11b938105ed8f903edfd
mk3mta <139065300+mk3mta@users.noreply.github.com>
2023-07-20 15:03:27 +0200

    Create meteo.php

---
+++ EE09-styczen-2020-zad-03/meteo.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div id=baner>
+        <div id=lewy>
+
+        <div id=środkowy>
+
+        <div id=prawy>
+
+    <div id=kontent>
+        <div id=lewy>
+
+        <div id=prawy>
+
+        <div id=główny>
+
+    <div id=stopka>
+        
+</body>
+</html>
